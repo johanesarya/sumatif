60a5d21343263b3c066aade0719cf3354e3570a7
Johanes Arya <johanesjohanes021@gmail.com>
2025-11-25 01:13:00 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import TextType from "./components/TextType";
+import MagicBento from "./components/MagicBento";
+// import GlitchText from "./components/GlitchText";
+// import ShinyText from "./components/ShinyText";
+import FuzzyText from "./components/FuzzyText";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* Text Type with logo */}
+      <div className="pt-4 flex justify-center items-center">
+        <img src="src/assets/logo.png" className="size-10" />
+        <TextType
+          text={[
+            "Hai Siswa-Siswi SMK PL Tarcisius!",
+            "Kelas XII AKL dan XII MPLB",
+          ]}
+          typingSpeed={75}
+          pauseDuration={1500}
+          showCursor={true}
+          cursorCharacter="|"
+        />
+      </div>
+
+      {/* Magic Bento */}
+      <MagicBento
+        textAutoHide={true}
+        enableStars={true}
+        enableSpotlight={true}
+        enableBorderGlow={true}
+        enableTilt={true}
+        enableMagnetism={true}
+        clickEffect={true}
+        spotlightRadius={300}
+        particleCount={12}
+        glowColor="132, 0, 255"
+      />
+
+      <footer className="m-4 p-3">
+        <FuzzyText>
+          Copyright © 2025 Johanes Arya Pramesta Nugraha, S.Ak., S.Kom
+        </FuzzyText>
+      </footer>
+    </>
+  );
+}
+
+export default App;
